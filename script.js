dfb59b4b90df8ad1c291b3699f63f1c7155df5ad
fengchuan-xiangzi <13936916802@163.com>
2026-09-22 22:08:00 +0800

    替换 PROJECT 01 PDF 为最新版 project11.pdf，重渲染 p1-01~p1-20.webp，升级版本号

--- script.js
+++ script.js
@@ -447,7 +447,7 @@
     'assets/project-2-case.pdf': { prefix: 'p2', pages: 14, title: 'PROJECT 02 ✱ UI 方案' },
     'assets/project-3-case.pdf': { prefix: 'p3', pages: 37, title: 'PORTFOLIO ✱ 作品集汇总' },
   }
-  const PDF_ASSET_VERSION = '20260921-2204'
+  const PDF_ASSET_VERSION = '20260922-2210'
   let pdfMeta = null
   let pdfPageNum = 1
 
